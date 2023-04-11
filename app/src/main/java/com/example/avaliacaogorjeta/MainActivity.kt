3dbfa2b7e2ea0175732707aabaabac38c17370ef
RAQUEL CRISTINA ISENSEE DA SILVA <raquel.silva2@alunos.sc.senac.br>
2023-04-10 21:29:01 -0300

    amount & amount input + WIP seekbar

--- app/src/main/java/com/example/avaliacaogorjeta/MainActivity.kt
+++ app/src/main/java/com/example/avaliacaogorjeta/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.avaliacaogorjeta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.avaliacaogorjeta.ui.theme.AvaliacaoGorjetaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +27,7 @@ class MainActivity : ComponentActivity() {
             AvaliacaoGorjetaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    apresentacao()
                 }
             }
         }
@@ -38,3 +46,36 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun apresentacao(){
+    val amount = remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Amount",
+            style = MaterialTheme.typography.subtitle1
+        )
+
+        OutlinedTextField(
+            value = amount.value,
+            onValueChange = {
+                amount.value = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+        Text(
+            text = "Custom:",
+            style = MaterialTheme.typography.subtitle1
+        )
+
+    }
+
+}
+
+
+
